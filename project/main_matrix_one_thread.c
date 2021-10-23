3c51fa14a62c1e108cc6bb998a7a2fe5a0bdf75c
Andrey123815 <ivredomon@gmail.com>
2021-10-23 17:10:46 +0300

    error in cppcheck

--- project/main_matrix_one_thread.c
+++ project/main_matrix_one_thread.c
@@ -1,8 +1,8 @@
 #include "include/matrix_one_thread.h"
 
 int main() {
-    Matrix* m = create_matrix(2,2);
-    int source_array_size = m->row * m->col;
+    Matrix* m = create_matrix(2, 2);
+    const int source_array_size = 4;
     int source_array[source_array_size];
     for (int i = 0; i < source_array_size; ++i) {
         source_array[i] = i;
@@ -10,6 +10,7 @@ int main() {
     fill_matrix(m, source_array, source_array_size);
     print_matrix(m);
     print_matrix(transp(m));
-    Matrix* m_new = create_matrix_from_file("file.tst");
+    m = create_matrix_from_file("file.tst");
+    free_matrix(m);
     return 0;
 }
